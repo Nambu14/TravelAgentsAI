c536ff3ab3c1f00879eca33b03c1583549f0237c
Lucas <Lucas@desktop>
2014-05-21 13:13:21 -0300

    Crecimiento en CronogramaTransporte

--- src/Things/CronogramaTrasporte.java
+++ src/Things/CronogramaTrasporte.java
@@ -6,7 +6,10 @@
 
 package Things;
 
+import java.time.DayOfWeek;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  *
@@ -19,10 +22,9 @@ public class CronogramaTrasporte {
     public int capacidad;
     public static enum Comodidad {Ejecutivo, Cama, Semicama};
     public Comodidad comodidad;
-    public static enum Dias {Lunes, Martes, Miercoles, Jueves, Sabado, Domingo};
-    public ArrayList<Dias> salidas;
+    public ArrayList<DayOfWeek> salidas;
 
-    public CronogramaTrasporte(String origen, String destino, float precio, int capacidad, Comodidad comodidad, ArrayList<Dias> salidas) {
+    public CronogramaTrasporte(String origen, String destino, float precio, int capacidad, Comodidad comodidad, ArrayList<DayOfWeek> salidas) {
         this.origen = origen;
         this.destino = destino;
         this.precio = precio;
@@ -30,6 +32,63 @@ public class CronogramaTrasporte {
         this.comodidad = comodidad;
         this.salidas = salidas;
     }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public Comodidad getComodidad() {
+        return comodidad;
+    }
+
+    public void setComodidad(Comodidad comodidad) {
+        this.comodidad = comodidad;
+    }
+
+    public ArrayList<DayOfWeek> getSalidas() {
+        return salidas;
+    }
+
+    public void setSalidas(ArrayList<DayOfWeek> salidas) {
+        this.salidas = salidas;
+    }
     
+    public boolean askForDate(GregorianCalendar fecha){
+        for (DayOfWeek estedia: salidas){
+            if (((fecha.get(Calendar.DAY_OF_WEEK)-1)==estedia.getValue())||((fecha.get(Calendar.DAY_OF_WEEK)==0)&&(estedia.getValue()==7))){
+                return true;
+            }
+        }
+        return false;
+    
+    }
     
 }
